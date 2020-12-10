0b61462e4028209a37bc260a7d06c5464ce76753
ehurteau <64095953+ehurteau@users.noreply.github.com>
2020-12-10 12:07:56 -0600

    Update ExampleItem.java

--- CampusMap/app/src/main/java/com/example/campus_map/ExampleItem.java
+++ CampusMap/app/src/main/java/com/example/campus_map/ExampleItem.java
@@ -4,11 +4,15 @@ public class ExampleItem {
     private int mImageResource;
     private String mText1;
     private String mText2;
+    private String mText3;
+    private String mText4;
 
-    public ExampleItem(int imageResource, String text1, String text2) {
+    public ExampleItem(int imageResource, String text1, String text2, String text3, String text4) {
         mImageResource = imageResource;
         mText1 = text1;
         mText2 = text2;
+        mText1 = text3;
+        mText2 = text4;
     }
 
     public int getImageResource() {
@@ -21,5 +25,13 @@ public class ExampleItem {
 
     public String getText2() {
         return mText2;
+    }
+
+    public String getText3() {
+        return mText3;
+    }
+
+    public String getText4() {
+        return mText4;
     }
 }
